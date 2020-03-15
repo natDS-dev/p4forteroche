8b644add065f7e14a45c49d96ea6abca0d872e23
natDS-dev <natachadesmet@yahoo.fr>
2020-03-15 17:03:48 +0100

    gestion média queries

--- views/includes/subtitles_admin.php
+++ views/includes/subtitles_admin.php
@@ -1,6 +1,6 @@
 <!--ADMIN SUBTITLES-->
 <div class="admin_subtitles">
-    <img class="main_picture" id="admin_main_picture" src="https://media.giphy.com/media/iFU36VwXUd2O43gdcr/source.gif" alt="profil d'homme dessiné" />
+    <img class="main_picture" id="admin_main_picture" src="https://media.giphy.com/media/iFU36VwXUd2O43gdcr/source.gif" alt="gif homme qui tape à la machine à écrire" />
     <h2>Vous êtes connecté à l'espace administrateur<h2>
     <img class="goldpaint_separator" id="goldpaint_separator2" src="public/images/goldpaint_separator.png" alt="trace peinture or" />
     <h3>Ici, vous pouvez gérer le contenu de vos pages</h3>
