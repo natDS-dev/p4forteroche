f8750f4e610b6e35ee89cebf8384d37ddcdfa4eb
natDS-dev <natachadesmet@yahoo.fr>
2020-03-15 11:48:09 +0100

    ajout CSS

--- views/chapters_view.php
+++ views/chapters_view.php
@@ -80,7 +80,7 @@
                                 <h4>Posté par : <?= htmlspecialchars($comment['author_comment']) ?><br/>  le <?=  nl2br(htmlspecialchars($comment['date_comment_fr'])); ?> </h4>
                                 <h5><?= htmlspecialchars($comment['title_comment']) ?></h5>
                                 <p><?= htmlspecialchars($comment['content_comment']) ?></p>
-                                <p class="comment_status">
+                                <div class="comment_status">
                                     <?php if($comment['statut_user_comment'] =='posted'):?>
                                         <a class="report_link" href="index.php?action=toReportComment&id=<?= $comment['id']; ?>&chapter_id=<?= $onePost['id']; ?>">Signaler</a>
                                     <?php elseif($comment['statut_user_comment'] =='validated') :?>
@@ -88,7 +88,7 @@
                                     <?php else: ?>
                                         <p  class="reported_comment">Le commentaire a déjà été signalé</p>
                                     <?php endif; ?> 
-                                </p>                   
+                                </div>                   
                             </section>
                         <?php endforeach; ?>
                     <?php else : ?>
